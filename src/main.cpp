f5e4e5fef2115ddde880c24e227c36562ff07d11
L <mateusz.les222@gmail.com>
2023-11-08 22:07:04 +0100

    tests

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,14 @@ int main() {
         "// Everyone \n"
         "// This is a /*string*/hmm :) \n"
         "if you like it or not as i do lul \n"
-        "3 + 5 = 3 += 4 ?? .. huh?"
+        "3 + 5 = 3 += 4 ?? .. huh? /*"
     );
     std::cout << lexer.GetSource() << std::endl;
     std::cout << std::string(20, '-') << std::endl;
     std::vector<ry::Token> tokens = lexer.Lex();
     for(const ry::Token& token : tokens)
         std::cout << token.Stringify() << std::endl;
+    std::cout << std::string(20, '-') << std::endl;
+    for(const ry::Lexer::Info& info : lexer.GetInfos())
+        std::cout << info.GetMessage() << std::endl;
 }
