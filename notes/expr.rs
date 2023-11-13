7153ae91b6d29e58a36ddb73761cf0eaff56e3ab
kvbc <mateusz.les222@gmail.com>
2023-11-13 12:51:55 +0100

    spec expr

---
+++ notes/expr.rs
@@ -0,0 +1,12 @@
+x := 3;
+y := 5;
+
+if x * y == 15 then {}
+else if x * y == 20 then {}
+else if x * y == 50 then {}
+else {}
+
+x * y == 15 and {}
+or x * y == 20 and {}
+or x * y == 50 and {}
+or {}
